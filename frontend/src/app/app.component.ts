956ac6f2faef6c74748d73af1991c52de1a04343
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-08-25 02:03:08 -0400

    added front end stuff

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from './spotify.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'frontendboiler';
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit() {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('code')) {
+      console.log(params.get('code'));
+      console.log('hit me');
+      this.spotifyService.getCodes(params.get('code')).subscribe(
+        this.onGetCodesSuccess.bind(this),
+        this.onGetCodesError.bind(this)
+      );
+    } else {
+      window.location.href = 'http://localhost:8888/login';
+    }
+  }
+
+  onGetCodesSuccess(codes: any): void {
+    console.log('hit me')
+    console.log(codes);
+    localStorage.setItem('access_token', codes.access_token);
+    this.spotifyService.setAccessToken(codes.access_token);
+    this.spotifyService.setRefreshToken(codes.refresh_token);
+  }
+
+  onGetCodesError(error: Error) {
+    console.log(error.message);
+  }
+
+  logStuff(): void {
+    console.log(this.spotifyService.getAccessToken());
+    console.log(this.spotifyService.getRefreshToken());
+  }
+
+  trySearchByCall() {
+    this.spotifyService.getSearchResults().subscribe(res => console.log(res));
+  }
+}
